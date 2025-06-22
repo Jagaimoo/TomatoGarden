6dbff67bab0143409721a9fa36ecf85bb24b38f6
“Jagaimoo” <Matteocheng2004@gmail.com>
2025-06-22 15:27:38 +0200

    calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,23 +1,47 @@
-
-
 function generaCalendario(year, month) {
-    const griglia = document.getElementById('griglia');
-    griglia.innerHTML = '';
+  const griglia = document.getElementById("griglia");
+  griglia.innerHTML = "";
 
-    const data = new Date(year, month, 1);
-    const start = data.getDay();  // giorno della settimana
+  const data = new Date(year, month, 1);
+  const start = data.getDay();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-    for (let i = 0; i < start; i++) {
-        griglia.innerHTML += `<div class="giorno vuoto"></div>`;
-    }
+  let monthName = document.getElementById("month");
 
-    while (data.getMonth() === month) {
-        griglia.innerHTML += `<div class="giorno" data-giorno="${data.getDate()}">${data.getDate()}</div>`;
-        data.setDate(data.getDate() + 1);
-    }
+  monthName.innerHTML = monthNames[data.getMonth()];
+
+  for (let i = 0; i < start; i++) {
+    griglia.innerHTML += `<div class="giorno vuoto"></div>`;
+  }
+
+  while (data.getMonth() === month) {
+    let rndimg = Math.floor(Math.random() * 4) + 1;
+    griglia.innerHTML += `<div class="giorno rubik-marker-hatch-regular" style="background:url(../img/Tile_${rndimg}.png);background-repeat: no-repeat;background-size: cover;" 
+    data-giorno="${data.getDate()}">${data.getDate()}</div>`;
+    data.setDate(data.getDate() + 1);
+  }
+  $("#prev").on("click", function () {
+    generaCalendario(year, month - 1);
+  });
+  $("#next").on("click", function () {
+    generaCalendario(year, month + 1);
+  });
 }
 
 $(document).ready(function () {
-    let today = new Date();
-    generaCalendario(parseInt(today.getFullYear()), parseInt(today.getMonth()));
+  let today = new Date();
+  generaCalendario(parseInt(today.getFullYear()), parseInt(today.getMonth()));
 });
